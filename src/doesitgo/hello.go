2f0e1e18a947f464e704a3b671c8b74d54096cef
AchroDev <achrodev@proton.me>
2024-05-26 17:13:56 -0500

    Tested adding an array for a list of strings

--- src/doesitgo/hello.go
+++ src/doesitgo/hello.go
@@ -3,5 +3,11 @@ package main
 import "fmt"
 
 func main() {
+	var list [3]string
+	list[0] = "site1"
+	list[1] = "site2"
+	list[2] = "site3"
+	
 	fmt.Println("Hello, world!")
+	fmt.Println(list[1])
 }
